2e937a373d0a99547337421de557ca0137488e55
Tomek <tomeksporczyk@gmail.com>
2019-08-26 13:22:24 +0200

    one tool for all Advice objects

--- driver/tests.py
+++ driver/tests.py
@@ -8,7 +8,7 @@ from django.test import TestCase, Client
 
 # Create your tests here.
 from driver.models import Advice
-from driver.tests_tools import create_image, create_weeks_advice, create_advice, create_passed_advice, create_user
+from driver.tests_tools import create_image, create_advice, create_user
 
 
 class ToolsTestCase(TestCase):
@@ -17,48 +17,45 @@ class ToolsTestCase(TestCase):
         image = create_image('test.jpg', '/home/tomek/PycharmProjects/Driver/media/advice/harold.jpg')
         self.assertEqual(image.name, 'test.jpg')
 
+    def setUp(self):
+        self.file = create_image('test_image.jpg',
+                                 '/home/tomek/PycharmProjects/Driver/media/advice/harold.jpg')
+        self.user = create_user()
+
     def test_weeks_advice(self):
-        file_name = 'test_week_advice.jpg'
-        dir = '/home/tomek/PycharmProjects/Driver/media/advice/harold.jpg'
-        tag_name = 'weeks advice tag'
-        create_weeks_advice(file_name, dir, tag_name)
+        create_advice('weeks', self.file,)
         advice = Advice.objects.get(weeks_advice=True)
         self.assertEqual(advice.title, "Week's advice")
 
     def test_normal_advice(self):
-        file_name = 'test_normal_advice.jpg'
-        dir = '/home/tomek/PycharmProjects/Driver/media/advice/harold.jpg'
-        tag_name = 'normal advice tag'
-        create_advice(file_name, dir, tag_name)
+        create_advice('normal', self.file,)
         advice = Advice.objects.get(lead='Normal lead')
         self.assertEqual(advice.title, "Normal advice")
 
-    def test_create_user(self):
-        create_user()
-        user = User.objects.first()
-        self.assertEqual(user.username, 'testuser')
-
     def test_passed_advice(self):
-        user = create_user()
-        file_name = 'test_passed_advice.jpg'
-        dir = '/home/tomek/PycharmProjects/Driver/media/advice/harold.jpg'
-        tag_name = 'passed advice tag'
-        create_passed_advice(user, file_name, dir, tag_name)
-        advice = Advice.objects.get(passed__username=user.username)
+        create_advice('passed', self.file, self.user)
+        advice = Advice.objects.get(passed__username=self.user.username)
         self.assertEqual(advice.title, "Passed advice")
 
 
-class HomeTestCase(TestCase):
+# class HomeTestCase(TestCase):
+#
+#     def test_empty_advice_table_status_handling(self):
+#         """
+#         :return: response 404 if no Advice objects were created
+#         """
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 404)
+#
+#     def setUp(self):
+#         self.image = create_image('test_image.jpg',
+#                      '/home/tomek/PycharmProjects/Driver/media/advice/harold.jpg')
+#         self.user = create_user()
+#         create_advice('weeks', self.image)
+#         create_advice('normal', self.image)
+#         create_advice('passed', self.image, self.user)
 
-    def test_empty_advice_table_status_handling(self):
-        """
-        :return: response 404 if no Advice objects were created
-        """
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 404)
 
-    def setUp(self):
-        create_image()
 
 if __name__ == "__main__":
     unittest.main()
--- driver/tests_tools.py
+++ driver/tests_tools.py
@@ -26,65 +26,49 @@ def create_image(file_name, dir):
     return file
 
 
-def create_weeks_advice(file_name, dir, tag_name):
+def create_advice(advice_type, file, user=None):
     """
-    todo: create one create_advice function for all advice types
-    :param file_name: string - name of media file to upload
-    :param dir: string - directory of the file
-    :param tag_name: string - name of the tag to be created
-    :return: creates Advice object with weeks_advice set to True
-    """
-    tag = create_tag(tag_name)
-    advice = Advice.objects.create(
-                        title="Week's advice",
-                        lead="Week's lead",
-                        article="Week's article",
-                        media=create_image(file_name, dir),
-                        weeks_advice=True,
-                        )
-    advice.tags.add(tag)
-    return advice
 
-
-def create_advice(file_name, dir, tag_name):
+    :param advice_type: sting = 'weeks' or 'normal' or 'passed
+    :param file: file object = created with create_image function
+    :param user: user instance = created with create_user function. Leave as None if not creating passed_advice
+    :return: an instance of an Advice - week's advice, normal, or passed
     """
-    :param file_name: string - name of media file to upload
-    :param dir: string - directory of the file
-    :param tag_name: string - name of the tag to be created
-    :return: creates Advice object
-    """
-    tag = create_tag(tag_name)
-    advice = Advice.objects.create(
-        title="Normal advice",
-        lead="Normal lead",
-        article="Normal article",
-        media=create_image(file_name, dir),
-        weeks_advice=False,
-    )
-    advice.tags.add(tag)
-    advice.save()
-    return advice
-
-
-def create_passed_advice(user, file_name, dir, tag_name):
-    """
-    :param user: user instance
-    :param file_name: string - name of media file to upload
-    :param dir: string - directory of the file
-    :param tag_name: string - name of the tag to be created
-    :return: creates Advice object with passed field including the user
-    """
-    tag = create_tag(tag_name)
-    advice = Advice.objects.create(
-        title="Passed advice",
-        lead="Passed lead",
-        article="Passed article",
-        media=create_image(file_name, dir),
-        weeks_advice=False,
-    )
-    advice.passed.add(user)
-    advice.save()
-    advice.tags.add(tag)
+    if advice_type == 'weeks':
+        tag = create_tag('weeks_advice')
+        advice = Advice.objects.create(
+            title="Week's advice",
+            lead="Week's lead",
+            article="Week's article",
+            media=file,
+            weeks_advice=True,
+            )
+        advice.tags.add(tag)
+    elif advice_type == 'normal':
+        tag = create_tag('normal_advice')
+        advice = Advice.objects.create(
+            title="Normal advice",
+            lead="Normal lead",
+            article="Normal article",
+            media=file,
+            weeks_advice=False,
+        )
+        advice.tags.add(tag)
+        advice.save()
+    elif advice_type == 'passed':
+        tag = create_tag('passed_advice')
+        advice = Advice.objects.create(
+            title="Passed advice",
+            lead="Passed lead",
+            article="Passed article",
+            media=file,
+            weeks_advice=False,
+            )
+        advice.passed.add(user)
+        advice.save()
+        advice.tags.add(tag)
+    else:
+        pass
     return advice
 
 
