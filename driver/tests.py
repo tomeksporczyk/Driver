c8aeb74d8bd4e00be0008cfb74945846bd313082
Tomek <tomeksporczyk@gmail.com>
2019-08-26 16:44:54 +0200

    No Advice objects test case

--- driver/tests.py
+++ driver/tests.py
@@ -1,6 +1,8 @@
+import os
 import unittest
 import unittest.mock
 
+from django.conf import settings
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.http import Http404
@@ -38,23 +40,36 @@ class ToolsTestCase(TestCase):
         self.assertEqual(advice.title, "Passed advice")
 
 
-# class HomeTestCase(TestCase):
-#
-#     def test_empty_advice_table_status_handling(self):
-#         """
-#         :return: response 404 if no Advice objects were created
-#         """
-#         response = self.client.get('/')
-#         self.assertEqual(response.status_code, 404)
-#
-#     def setUp(self):
-#         self.image = create_image('test_image.jpg',
-#                      '/home/tomek/PycharmProjects/Driver/media/advice/harold.jpg')
-#         self.user = create_user()
-#         create_advice('weeks', self.image)
-#         create_advice('normal', self.image)
-#         create_advice('passed', self.image, self.user)
+class HomeAdvicelessTestCase(TestCase):
+    """
+    get the home page with no Advice objects created
+    """
+    def test_empty_advice_table_status_handling(self):
+        """
+        :return: response 404 if no Advice objects were created
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 404)
 
+        
+class HomeTestCase(TestCase):
+
+    def setUp(self):
+        self.image = create_image('test_image.jpg',
+                     '/home/tomek/PycharmProjects/Driver/media/advice/harold.jpg')
+        self.user = create_user()
+        self.weeks = create_advice('weeks', self.image)
+        self.normal = create_advice('normal', self.image)
+        self.passed = create_advice('passed', self.image, self.user)
+
+    def tearDown(self):
+        os.remove(self.weeks.media.path)
+        os.remove(self.normal.media.path)
+        os.remove(self.passed.media.path)
+
+    def test_home_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
